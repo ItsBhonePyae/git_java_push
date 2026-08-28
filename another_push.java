0a352113ffc796a01ffda0d8827d770119de2ec3
Bhone Pyae Maung <phozan2007@gmail.com>
2026-08-28 15:00:28 +0630

    another push for java

---
+++ another_push.java
@@ -0,0 +1,11 @@
+package Git_test;
+
+public class another_push {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the another push");
+
+	}
+
+}
