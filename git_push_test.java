17951c79518cd90d6aee924e1fa65201cf9c0b52
Bhone Pyae Maung <phozan2007@gmail.com>
2026-08-28 14:55:56 +0630

    Java git push

---
+++ git_push_test.java
@@ -0,0 +1,10 @@
+package Git_test;
+
+public class git_push_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World this is java git push test");
+	}
+
+}
