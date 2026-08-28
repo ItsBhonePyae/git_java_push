8f1ce82f35c2b9b64c8e3ad99f511c5eea5b7904
Bhone Pyae Maung <phozan2007@gmail.com>
2026-08-28 15:04:36 +0630

    This is pushing into main branch

---
+++ into_main.java
@@ -0,0 +1,10 @@
+package Git_test;
+
+public class into_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is to the main branch");
+	}
+
+}
